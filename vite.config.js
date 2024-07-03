aeac3cc7d0e135b4d77e4fc1b5d66c70b60003a2
Aradhana Kharwar <kharwararadhana2001@gmail.com>
2024-07-03 10:05:07 +0530

    changes for storage

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    chunkSizeWarningLimit: 500,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
-  }
-});
+  plugins: [react()],
+})
